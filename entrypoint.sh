8e2052a94da52db67dfdf4ea0de27eec0ce8f10e
Robin Quintero <rohaquinlop301@gmail.com>
2025-06-01 23:29:02 -0500

    Refactor action.yml and entrypoint.sh for enhanced argument handling
    - Updated action.yml to simplify the passing of command-line arguments by directly using input values without conditional formatting.
    - Modified entrypoint.sh to construct command-line arguments for complexipy based on input parameters, improving clarity and maintainability.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 echo "Running complexipy with arguments: $@"
 
-complexipy "$@"
+output_file=$( [ "$3" = "true" ] && '-o' || '' )
+details=$( [ "$4" = "low" ] && '-d low' || '-d normal' )
+quiet=$( [ "$5" = "true" ] && '-q' || '' )
+sort=$( [ "$6" = "asc" ] && '-s asc' || [ "$6" = "desc" ] && '-s desc' || [ "$6" = "name" ] && '-s name' || '' )
+
+complexipy $1 -c $2 $output_file $details $quiet $sort
